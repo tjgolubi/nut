76c08be54c169d99024d5c888ba91579d55cea5b
Terry Golubiewski <tjgolubi@netins.net>
2024-01-03 22:53:17 -0600

    Lookup does portions.

--- lookup.cpp
+++ lookup.cpp
@@ -9,10 +9,16 @@
 #include <chrono>
 #include <limits>
 #include <cstdlib>
+#include <cmath>
 
 const std::string UsdaPath = "c:/Users/tjgolubi/prj/usda/";
 const std::string FdcPath  = UsdaPath + "fdc/";
 const std::string SrPath   = UsdaPath + "sr/";
+
+std::ios::fmtflags DefaultCoutFlags;
+
+constexpr auto Round(float x) -> float
+  { return (std::abs(x) < 10) ? (std::round(10 * x) / 10) : std::round(x); }
 
 template<class E>
 struct ParseVec: public std::vector<std::string> {
@@ -152,21 +158,6 @@ auto ReadAtwaterFoods(StringDb& atwaterDb)
   return atwaterFoods;
 } // ReadAtwaterFoods
 
-struct FormatSaver {
-  std::ostream& os;
-  std::ios::fmtflags flags;
-  std::streamsize prec;
-  std::streamsize width;
-  explicit FormatSaver(std::ostream& os_)
-    : os{os_}, flags{os_.flags()}, prec{os_.precision()}, width{os_.width()}
-    { }
-  ~FormatSaver() {
-    os.flags(flags);
-    os.precision(prec);
-    os.width(width);
-  }
-}; // FormatSaver
-
 enum class FieldIdx {
   kJ, energy, atwater_general, atwater_specific, protein, fat,
   carb_diff, carb_sum, fiber, alcohol, end
@@ -227,16 +218,16 @@ std::ostream& operator<<(std::ostream& os, const Ingred& f) {
   auto fiber = -f.alcohol();
   if (fiber == 0.0)
     fiber = f.fiber();
-  FormatSaver saver(os);
+  std::ostringstream ostr;
   using namespace std;
-  os << fixed;
-  os.precision(2);
-  os << fixed << setw(7) << f.energy()
-     <<   ' ' << setw(7) << f.protein()
-     <<   ' ' << setw(7) << f.fat()
-     <<   ' ' << setw(7) << f.carb()
-     <<   ' ' << setw(7) << fiber;
-  return os << ' ' << f.desc;
+  ostr << setw(5) << Round(f.energy())
+       << fixed << setprecision(2)
+       << ' ' << setw(6) << f.protein()
+       << ' ' << setw(6) << f.fat()
+       << ' ' << setw(6) << f.carb()
+       << ' ' << setw(6) << fiber
+       << ' ' << f.desc;
+  return os << ostr.str();
 } // << Ingred
 
 auto ReadFoods(const std::map<std::string, int>& atwaterFoods)
@@ -316,7 +307,121 @@ void UpdateAtwaterFromLegacy(std::map<std::string, Ingred>& foods,
   }
 } // UpdateAtwaterFromLegacy
 
+float ConversionFactor(const std::string& unit) {
+  constexpr float Cup = 236.6;
+  static const std::map<std::string, float> FactorMap = {
+    { "cup",        Cup     },
+    { "tablespoon", Cup/16  },
+    { "teaspoon",   Cup/48  },
+    { "liter",      1000.0  },
+    { "milliliter", 1.0     },
+    { "cubic inch", 16.39   },
+    { "cubic centimeter", 1.0 },
+    { "gallon",     16 * Cup },
+    { "pint",        2 * Cup },
+    { "fl oz",       Cup / 8 },
+    { "quart",       4 * Cup },
+    { "Tablespoons", Cup/16  }
+  }; // FactorMap
+  if (unit.empty())
+    return 0.0f;
+  if (auto iter = FactorMap.find(unit); iter != FactorMap.end())
+    return iter->second;
+  return 0.0f;
+} // ConversionFactor
+
+struct Portion {
+  const Ingred& ingred;
+  float g  = 0.0;
+  float ml = 0.0;
+  std::string desc;
+  Portion(const Ingred& i, const std::string& n, float g_, float ml_)
+    : ingred{i}, desc(n), g{g_}, ml{ml_} { }
+}; // Portion
+
+std::ostream& operator<<(std::ostream& os, const Portion& p) {
+  return os << '{' << p.g << ' ' << p.ml << ' ' << p.desc << '}';
+} // << Portion
+
+auto LoadPortions(const std::map<std::string, Ingred>& foods)
+  -> std::multimap<std::string, Portion>
+{
+  struct Unit {
+    const std::string name;
+    float ml_factor = 0.0;
+    explicit Unit(const std::string& n)
+      : name{n}, ml_factor{ConversionFactor(n)} { }
+    Unit() { }
+  }; // Unit
+  static const Unit NullUnit;
+  std::map<std::string, Unit> units;
+  units.emplace("9999", NullUnit);
+  std::string line;
+  {
+    enum class Idx { id, name };
+    const auto fname = FdcPath + "measure_unit.csv";
+    auto input = std::ifstream(fname);
+    if (!input)
+      throw std::runtime_error("Cannot open " + fname);
+    std::getline(input, line); // discard headings
+    while (std::getline(input, line)) {
+      auto v = Parse<Idx>(line);
+      if (v[Idx::id] != "9999")
+	units.emplace(v[Idx::id], Unit(v[Idx::name]));
+    }
+    std::cout << "Loaded " << units.size() << " units of measure.\n";
+  }
+  std::multimap<std::string, Portion> rval;
+  {
+    enum class Idx { id, fdc_id, seq_num, amount, unit, desc,
+	modifier, grams, data_points, footnote, min_year_acquired };
+    const auto fname = FdcPath + "food_portion.csv";
+    auto input = std::ifstream(fname);
+    if (!input)
+      throw std::runtime_error("Cannot open " + fname);
+    std::getline(input, line); // discard headings
+    while (std::getline(input, line)) {
+      auto v = Parse<Idx>(line);
+      const auto& fdc_id = v[Idx::fdc_id];
+      auto food = foods.find(fdc_id);
+      if (food == foods.end())
+        continue;
+      const Ingred& ingred = food->second;
+      float ml = 0.0;
+      float g  = std::stof(v[Idx::grams]);
+      std::ostringstream desc;
+      const auto& amount = v[Idx::amount];
+      float val = amount.empty() ? 0.0 : std::stof(amount);
+      if (val != 0.0 && val != 1.0)
+        desc << val << 'x';
+      auto iter = units.find(v[Idx::unit]);
+      const auto& unit = (iter != units.end()) ? iter->second : NullUnit;
+      if (!unit.name.empty()) {
+        desc << unit.name;
+	ml = val * unit.ml_factor;
+      }
+      if (!v[Idx::desc].empty()) {
+        if (!desc.str().empty())
+	  desc << ' ';
+	desc << v[Idx::desc];
+      }
+      if (!v[Idx::modifier].empty()) {
+        if (!desc.str().empty())
+	  desc << ' ';
+	desc << v[Idx::modifier];
+      }
+      if (!desc.str().empty())
+        desc << ' ';
+      desc << ingred.desc;
+      rval.emplace(fdc_id, Portion{ingred, desc.str(), g, ml});
+    }
+  }
+  std::cout << "Loaded " << rval.size() << " portions.\n";
+  return rval;
+} // LoadPortions
+
 int main() {
+  DefaultCoutFlags = std::cout.flags();
   std::cout << "Starting..." << std::endl;
   StringDb atwaterDb;
 
@@ -364,6 +469,7 @@ int main() {
     food->second.values.at(i) = std::stof(amount);
   }
   std::cout << "\r100% complete\n";
+  const auto portions = LoadPortions(foods);
   std::multimap<int, const Ingred*> group;
   for (const auto& [id, ingred]: foods)
     group.emplace(ingred.atwater, &ingred);
@@ -374,7 +480,22 @@ int main() {
       last_atwater = id;
       std::cout << '[' << atwaterDb.str(id) << "]\n";
     }
-    std::cout << "    100     0 " << (*ingred) << " // " << ingred->id << '\n';
+    std::cout << "   100     0 " << (*ingred)
+              << " // usda " << ingred->id << '\n';
+    {
+      auto r = portions.equal_range(ingred->id);
+      using std::setw, std::left, std::right, std::quoted;
+      std::ostringstream ostr;
+      for (auto it = r.first; it != r.second; ++it) {
+        const auto& p = it->second;
+	ostr << '*' << setw(5) << Round(p.g)
+	     << ' ' << setw(5) << Round(p.ml)
+	     << ' ' << setw(5) << Round(p.ingred.energy() * p.g / 100)
+	     << ' ' << left << setw(27) << quoted(p.ingred.desc)
+	     << right << ' ' << p.desc << '\n';
+      }
+      std::cout << ostr.str();
+    }
   }
   return 0;
 } // main
